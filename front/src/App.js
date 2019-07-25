6e3ec7020e1e621c31325452d6bd085a0140500c
BaselBarghouth <baselbarghout0@gmail.com>
2019-07-25 16:02:15 +0300

    add customer and search

--- front/src/App.js
+++ front/src/App.js
@@ -1,22 +1,18 @@
-import React, { Component } from 'react';
-import Landing from './pages/Landing/Landing.js';
+import React, { Component } from "react";
+import AddCustomerCms from "./components/Add-new-customer-cms/Add-new-customer-cms.js";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      
-    }
+    this.state = {};
   }
 
-
-  render() { 
-
+  render() {
     return (
-      <div>
-        <Landing />
+      <div className="App">
+        <AddCustomerCms />
       </div>
-    )
+    );
   }
-}  
+}
 export default App;
--- front/src/components/Add-new-customer-cms/Add-new-customer-cms.js
+++ front/src/components/Add-new-customer-cms/Add-new-customer-cms.js
@@ -1,1 +1,96 @@
- 
+import React, { Component } from "react";
+import "./Add-new-customer-cms.css";
+class AddCustomerCms extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isUser: false
+    };
+  }
+
+  handleClick = event => {
+    this.setState({
+      isUser: !this.state.isUser
+    });
+  };
+  render() {
+    return (
+      <div className="AddFormUser">
+        <div className="Seach-for-user">
+          <form class="form-inline my-2 my-lg-0">
+            <input
+              class="form-control mr-sm-2"
+              type="search"
+              placeholder="Search"
+            />
+            <button class="btn btn-outline-primary" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+        <div className="Add-user-button">
+          <button
+            type="button"
+            class="btn btn-outline-primary"
+            onClick={this.handleClick}
+          >
+            Add User
+          </button>
+        </div>
+        {this.state.isUser ? (
+          <div className="Add-User-Form">
+            <div class="form-group">
+              <input
+                type="text"
+                class="form-control"
+                id="exampleInputEmail1"
+                placeholder="Name"
+              />
+            </div>
+            <div class="form-group">
+              <input
+                type="text"
+                class="form-control"
+                id="exampleInputEmail1"
+                placeholder="Last Name"
+              />
+            </div>
+            <div class="form-group">
+              <input
+                type="text"
+                class="form-control"
+                id="exampleInputEmail1"
+                placeholder="Middle Name"
+              />
+            </div>
+            <div class="form-group">
+              <input
+                type="text"
+                class="form-control"
+                id="exampleInputEmail1"
+                placeholder="Gender"
+              />
+            </div>
+            <div class="form-group">
+              <input
+                type="text"
+                class="form-control"
+                id="exampleInputEmail1"
+                placeholder="Addres"
+              />
+            </div>
+            <div className="Add-user-button">
+              <button type="button" class="btn btn-outline-primary">
+                Add
+              </button>
+            </div>
+          </div>
+        ) : (
+          false
+        )}
+      </div>
+    );
+  }
+}
+
+export default AddCustomerCms;
